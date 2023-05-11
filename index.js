bcaef2977f309649c64e10f7c0c067c40979e72f
Yoonanna <Yanykovaanya@gmail.com>
2023-05-11 17:49:43 +0200

    feat: implement push on mine

--- index.js
+++ index.js
@@ -22,6 +22,29 @@ game.append(field);
 function start(width, heigth, minesCount) {
     let cells = width * heigth;
     field.innerHTML = '<button></button>'.repeat(cells);
+
+    let fieldCells = [...field.children];
+    let mines = [...Array(cells).keys()]
+        .sort(() => Math.random() - 0.5)
+        .slice(0, minesCount);
+
+    field.addEventListener('click', (event) => {
+        if (event.target.tagName !== 'BUTTON')
+            return;
+        let index = fieldCells.indexOf(event.target);
+        let column = index % width;
+        let row = Math.floor(index / width);
+        open(row, column);
+
+    });
+    function open(row, column) {
+        let index = row * width + column;
+        let cell = fieldCells[index];
+        cell.innerHTML = isMine(row, column) ? '\u{1F4A3}' : ' ';
+    }
+    function isMine(row, column) {
+        let index = row * width + column;
+        return mines.includes(index);
+    }
 };
 start(10, 10, 10);
-
