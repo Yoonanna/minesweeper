8e2bf57f24bef8a93e9fd465ca9c22e4b640750a
Yoonanna <Yanykovaanya@gmail.com>
2023-05-18 18:49:17 +0200

    feat: add counter,modal and timer

--- index.js
+++ index.js
@@ -14,22 +14,28 @@ wrapper.append(game);
 let info = document.createElement('div');
 info.className = 'info';
 game.append(info);
+
 let counter = document.createElement('div');
 counter.className = 'counter';
-counter.innerText = '010';
+counter.innerText = '0';
 info.append(counter);
+
 let smile = document.createElement('div');
 smile.className = 'smile';
-smile.innerText = '\u{1F604}';
+smile.innerText = 'new game';
 info.append(smile);
+
 let timer = document.createElement('div');
 timer.className = 'timer';
-timer.innerText = 'time';
 info.append(timer);
 
 let field = document.createElement('div');
 field.className = 'field';
 game.append(field);
+
+let modal = document.createElement('div');
+modal.className = 'modal';
+wrapper.append(modal);
 
 function start(width, heigth, minesCount) {
     let cells = width * heigth;
@@ -47,16 +53,46 @@ function start(width, heigth, minesCount) {
         let column = index % width;
         let row = Math.floor(index / width);
         open(row, column);
+        counter.innerText++;
 
     });
     function open(row, column) {
         let index = row * width + column;
         let cell = fieldCells[index];
         cell.innerHTML = isMine(row, column) ? '\u{1F4A3}' : ' ';
+        if (cell.innerHTML == '\u{1F4A3}') {
+            modal.classList.add('active');
+            modal.innerText = 'Game over... Try again! =)';
+            clearInterval(setTime);
+        }
+
     }
     function isMine(row, column) {
         let index = row * width + column;
+
         return mines.includes(index);
+
     }
 };
 start(10, 10, 10);
+
+let setTime;
+let sec = 0;
+let min = 0;
+timer.innerHTML = '000';
+function tick() {
+    sec++;
+
+    if (sec < 10) { timer.innerHTML = '00' + sec }
+    else if (sec >= 10 && sec < 100) { timer.innerHTML = '0' + sec }
+    else { timer.innerHTML = ' ' + sec }
+
+};
+
+field.addEventListener('click', function init() {
+    sec = 0;
+    setTime = setInterval(tick, 1000);
+    this.removeEventListener("click", arguments.callee);
+});
+
+smile.addEventListener('click', () => window.location.reload());
